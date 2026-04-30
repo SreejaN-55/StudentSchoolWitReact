b14dba308417c330e10200babcb13f5919159c20
SreejaN-55 <nksreeja55@gmail.com>
2026-04-30 13:03:32 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ChatInterface from './ChatInterface';
+import PdfChat from './PdfChat';
+import ExcelColumnSplitter from './ExcelColumnSplitter';
+
+function App() {
+  return (
+    <div className="App">
+      <ChatInterface/>
+    </div>
+  );
+}
+
+export default App;
